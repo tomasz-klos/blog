49fecbfb48c43415ae9e116057a06b5074b96481
Tomasz Klos <tomasz.klos@nerds.family>
2024-02-03 19:58:21 +0100

    feat: create basic new Blog Post view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'blog_posts/new', to: 'blog_posts#new', as: 'new_blog_post'
   get 'blog_posts/:id', to: 'blog_posts#show', as: 'blog_post'
+  post 'blog_posts', to: 'blog_posts#create', as: 'blog_posts'
 
   # Defines the root path route ("/")
-  root 'blog_posts#index', as: "blog_posts"
+  root 'blog_posts#index', as: 'blog_posts_root'
 end

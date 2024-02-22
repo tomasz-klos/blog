b43740f8bfb48998a6cf360aede267fd67effd48
Tomasz Klos <tomasz.klos@nerds.family>
2024-02-22 12:39:07 +0100

    refactor: create set_blog_post method in BlogPostController

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,11 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: %i[show edit update destroy]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -22,13 +23,9 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  def edit
-    @blog_post = BlogPost.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
-
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -36,13 +33,7 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  def delete
-    @blog_post = BlogPost.find(params[:id])
-  end
-
   def destroy
-    @blog_post = BlogPost.find(params[:id])
-
     if @blog_post.destroy
       redirect_to blog_posts_root_path
     else
@@ -54,5 +45,9 @@ class BlogPostsController < ApplicationController
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
   end
 end

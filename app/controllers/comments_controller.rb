a91ed695d1050c10f408d31b8779d839a1bacffe
Tomasz Klos <tomasz.klos@nerds.family>
2024-02-24 21:52:58 +0100

    feat: use turbo stream for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,20 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-  
+
   def create
-  @blog_post = BlogPost.find(params[:blog_post_id])
-  @comment = @blog_post.comments.build(comment_params)
-  @comment.user_id = current_user.id
-  
+    @blog_post = BlogPost.find(params[:blog_post_id])
+    @comment = @blog_post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
     respond_to do |format|
       if @comment.save
-        format.turbo_stream { render turbo_stream: turbo_stream.prepend('comments', partial: 'comments/comment', locals: { comment: @comment }) }
+        format.turbo_stream do
+          turbo_stream.prepend('comments', partial: 'comments/comment', locals: { comment: @comment })
+        end
       else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('comment-form', partial: 'comments/form', locals: { comment: @comment }) }
+        format.turbo_stream do
+          turbo_stream.replace(@comment, partial: 'comments/form', locals: { comment: @comment })
+        end
       end
     end
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,13 @@
 class Comment < ApplicationRecord
-  belongs_to :blog_post, counter_cache: :comments_count 
-  belongs_to :parent_comment, class_name: "Comment", optional: true
+  after_create_commit { broadcast_prepend_to 'comments' }
+  after_update_commit { broadcast_replace_to 'comments' }
+  after_destroy_commit { broadcast_remove_to 'comments' }
+
+  belongs_to :blog_post, counter_cache: :comments_count
+  belongs_to :parent_comment, class_name: 'Comment', optional: true
   belongs_to :user
-  
-  has_many :replies, class_name: "Comment", foreign_key: "parent_comment_id", dependent: :destroy
+
+  has_many :replies, class_name: 'Comment', foreign_key: 'parent_comment_id', dependent: :destroy
 
   has_rich_text :content
   validates :content, presence: true

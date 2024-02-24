08245a5d07e6e4940e0d298428f333dc99302579
Tomasz Klos <tomasz.klos@nerds.family>
2024-02-24 17:49:47 +0100

    feat: create routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :blog_posts
+  resources :blog_posts do
+    resources :comments, only: [:create, :destroy]
+  end
 
   # Defines the root path route ("/")
   root 'blog_posts#index', as: 'blog_posts_root'

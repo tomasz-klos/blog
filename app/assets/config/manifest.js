9623e61a5a7254275080ecf4bb915aafb9f43418
Tomasz Klos <tomasz.klos@nerds.family>
2024-02-25 18:41:37 +0100

    fix: problem with file application.tailwind.scss

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
-//= link_tree ../stylesheets
-//= link application.css
+//= link_tree ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds

d7865c111fd06f99f08b61ac02858f08199279ff
Tomasz Klos <tomasz.klos@nerds.family>
2024-03-01 19:42:14 +0100

    refactor: custom_toggle_controller.js

--- app/javascript/controllers/custom_toggle_controller.js
+++ app/javascript/controllers/custom_toggle_controller.js
@@ -1,19 +1,23 @@
 import { Toggle } from "tailwindcss-stimulus-components";
 
 export default class CustomToggle extends Toggle {
-  static targets = ["hide", "show"];
+ static targets = ["hide", "show"];
 
-  connect() {
+ connect() {
     super.connect();
-  }
+    this.updateToggleState();
+ }
 
-  toggle(event) {
+ toggle(event) {
     super.toggle(event);
+    this.updateToggleState();
+ }
 
+ updateToggleState() {
     const isOpen = this.data.scope.element.hasAttribute("data-custom-toggle-open-value") &&
       this.data.scope.element.getAttribute("data-custom-toggle-open-value") === "true";
 
     this.showTarget.classList.toggle("hidden", isOpen);
     this.hideTarget.classList.toggle("hidden", !isOpen);
-  }
+ }
 }

2e3c1419e2781081d5c899200ec37506665aff68
Tomasz Klos <tomasz.klos@nerds.family>
2024-03-12 13:26:06 +0100

    feat: update dropdown component

--- app/components/common/dropdown/component.rb
+++ app/components/common/dropdown/component.rb
@@ -1,2 +1,12 @@
 class Common::Dropdown::Component < ApplicationComponent
+  option :position, default: -> { 'center' }, optional: true
+
+  def menu_position
+    case position
+    when 'left'
+      'right-0'
+    when 'right'
+      'left-0'
+    end
+  end
 end

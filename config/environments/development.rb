9a6341a5631233f1a86486ed56db25f7241ce0a5
Tomasz Klos <tomasz.klos@nerds.family>
2024-03-12 15:56:43 +0100

    feat: dd new fonts and update stylesheets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
 
   config.assets.debug = true
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 

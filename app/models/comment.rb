20ad7b8fcaeee8e736c0232877bc28d5310d2186
Tomasz Klos <tomasz.klos@nerds.family>
2024-03-29 20:48:58 +0100

    fix: rendering issue and add dependent: :destroy to likes association in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   include Likable
+
   after_create_commit { broadcast_append_to 'comments' }
   after_update_commit { broadcast_replace_to 'comments' }
   after_destroy_commit { broadcast_remove_to 'comments' }
@@ -8,12 +9,8 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   has_many :replies, class_name: '::Reply', foreign_key: 'comment_id', dependent: :destroy
-  has_many :likes, as: :likable
+  has_many :likes, as: :likable, dependent: :destroy
 
   has_rich_text :content
   validates :content, presence: true
-
-  def liked_by?(user)
-    likes.exists?(user:)
-  end
 end

c6b139721fdc06b81668f31df960810bf57a2f02
Tomasz Klos <tomasz.klos@nerds.family>
2024-03-29 20:41:29 +0100

    fix: adjust schema.rb with likes_count column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_13_223748) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_29_193512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_223748) do
     t.bigint "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count"
     t.index ["comment_id"], name: "index_replies_on_comment_id"
     t.index ["user_id"], name: "index_replies_on_user_id"
   end

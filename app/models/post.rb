54fa6c84063d47d00ff4838c6024daa1ee81c524
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-04 22:12:26 +0200

    WIP

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  has_rich_text :content
+  has_many :comments, dependent: :destroy, counter_cache: :comments_count
+
+  validates :title, presence: true, length: { minimum: 5 }, uniqueness: true
+  validates :content, presence: true
+end

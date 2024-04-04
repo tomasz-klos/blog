3d7685badf1800f18450e454d3d3ff26e84f6e04
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-04 21:16:48 +0200

    refactor: adjust path for comments and simplify CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
   include ActionView::RecordIdentifier
 
   before_action :authenticate_user!, only: %i[create edit update destroy toggle_like]
-  before_action :set_blog_post, only: %i[create edit update destroy toggle_like]
   before_action :authorize_user!, only: %i[edit update destroy]
 
   def create
+    @blog_post = BlogPost.find(params[:blog_post_id])
     @comment = @blog_post.comments.build(comment_params)
     @comment.user_id = current_user.id
 
@@ -21,7 +21,7 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = @blog_post.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
 
     render(turbo_stream: turbo_stream.replace(dom_id(@comment, :content),
                                               partial: 'comments/form',
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
   end
 
   def update
-    @comment = @blog_post.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
 
     if @comment.update(comment_params)
       render(partial: 'comments/comment', locals: { comment: @comment })
@@ -39,7 +39,7 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = @blog_post.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
     @comment.destroy
 
     respond_to do |format|
@@ -50,7 +50,7 @@ class CommentsController < ApplicationController
   end
 
   def toggle_like
-    @comment = @blog_post.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
     @comment.toggle_like(current_user)
 
     render(partial: 'comments/comment', locals: { comment: @comment })
@@ -62,14 +62,8 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content)
   end
 
-  def set_blog_post
-    @blog_post = BlogPost.find(params[:blog_post_id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to(root_path)
-  end
-
   def authorize_user!
-    @comment = @blog_post.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
     return if @comment.user == current_user
 
     redirect_to(@blog_post, alert: 'You are not authorized to perform this action.')

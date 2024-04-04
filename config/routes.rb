4864e02e0cde88ac42344829b4187a0bf83d38de
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-04 21:54:31 +0200

    refactor: routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  concern :likable do
+    member do
+      post 'toggle_like'
+    end
+  end
+
   resources :blog_posts do
     resources :comments, only: %i[create]
   end
 
-  resources :comments, only: %i[edit update destroy] do
-    member do
-      post 'toggle_like'
-    end
-    resources :replies do
-      member do
-        post 'toggle_like'
-      end
-    end
+  resources :comments, only: %i[edit update destroy], concerns: :likable do
+    resources :replies, concerns: :likable
   end
 
   # Defines the root path route ("/")

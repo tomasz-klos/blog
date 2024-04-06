74ce71ae8c701a422a0188c456e67ec6b53d141e
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-06 16:50:16 +0200

    refactor: user model associations to use dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :likes
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end

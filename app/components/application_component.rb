c9f17f32fb9d4741a32b9cf34deb2ca6ab191c88
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-06 23:02:08 +0200

    feat: add dry-types gem

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,8 +1,7 @@
-require_relative '../types/types'
-
 class ApplicationComponent < ViewComponent::Base
   extend Dry::Initializer
   include IconHelper
   include Turbo::StreamsHelper
   include Turbo::FramesHelper
+  include Types
 end
--- app/types/types.rb
+++ app/types/types.rb
@@ -1,12 +1,5 @@
+require 'dry-types'
+
 module Types
-  def self.Instance(model_class)
-    lambda { |value|
-      if value.is_a?(model_class) || value.nil?
-        value
-      else
-        (raise ArgumentError,
-               "#{value} is not an instance of #{model_class}")
-      end
-    }
-  end
+  include Dry.Types()
 end

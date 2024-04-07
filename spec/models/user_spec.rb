038cfc659ac52f42194dec19fa4a268044288cd3
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-07 12:50:54 +0200

    tests: refactor user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it('should be valid') do
-    user = User.new(email: 'email@example.com', password: 'password')
-    expect(user).to be_valid
+  describe('validations') do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_length_of(:password).is_at_least(6) }
   end
 
-  it('should be invalid without an email') do
-    user = User.new(email: nil, password: 'password')
-    expect(user).not_to be_valid
-  end
-
-  it('should be invalid without a password') do
-    user = User.new(email: 'email@example.com', password: nil)
-    expect(user).not_to be_valid
-  end
-
-  it('should be invalid with a duplicate email') do
-    User.create(email: 'email@example.com', password: 'password')
-    user = User.new(email: 'email@example.com', password: 'password')
-    expect(user).not_to be_valid
-  end
-
-  it('should be invalid with a password less than 6 characters') do
-    user = User.new(email: 'email@example.com', password: 'pass')
-    expect(user).not_to be_valid
+  describe('associations') do
+    it { should have_many(:posts) }
+    it { should have_many(:likes) }
   end
 end

5a7da25f7698b48037c678df9956ea62e20b0b0b
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-07 12:51:22 +0200

    tests: create Post model spec and factory for testing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,46 +3,51 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   let!(:user) { FactoryBot.create(:user) }
 
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:content) }
+
   describe('validations') do
-    it('is valid with valid attributes') do
-      post = Post.new(user:, title: 'Test Post', content: 'Lorem ipsum' * 20)
-      expect(post).to be_valid
+    context('with valid attributes') do
+      let(:post) { FactoryBot.build(:post, user:, title: 'Test Post', content: 'Lorem ipsum' * 25) }
+
+      it { expect(post).to be_valid }
     end
 
-    it('is not valid without a title') do
-      post = Post.new(user:, content: 'Lorem ipsum' * 20)
-      expect(post).to_not be_valid
-    end
+    context('with invalid attributes') do
+      it('is not valid without a title') do
+        post = FactoryBot.build(:post, user:, title: nil)
+        expect(post).to_not be_valid
+      end
 
-    it('is not valid without content') do
-      post = Post.new(user:, title: 'Test Post')
-      expect(post).to_not be_valid
-    end
+      it('is not valid without content') do
+        post = FactoryBot.build(:post, user:, content: nil)
+        expect(post).to_not be_valid
+      end
 
-    it('is not valid without a user') do
-      post = Post.new(title: 'Test Post', content: 'Lorem ipsum' * 20)
-      expect(post).to_not be_valid
-    end
+      it('is not valid without a user') do
+        post = FactoryBot.build(:post, user: nil)
+        expect(post).to_not be_valid
+      end
 
-    it('is not valid with a title less than 5 characters') do
-      post = Post.new(user:, title: 'Test', content: 'Lorem ipsum' * 20)
-      expect(post).to_not be_valid
-    end
+      it('is not valid with a title less than 5 characters') do
+        post = FactoryBot.build(:post, user:, title: 'Test', content: 'Lorem ipsum' * 25)
+        expect(post).to_not be_valid
+      end
 
-    it('is not valid with a title more than 200 characters') do
-      post = Post.new(user:, title: 'a' * 201, content: 'Lorem ipsum' * 20)
-      expect(post).to_not be_valid
-    end
+      it('is not valid with a title more than 200 characters') do
+        post = FactoryBot.build(:post, user:, title: 'a' * 201, content: 'Lorem ipsum' * 25)
+        expect(post).to_not be_valid
+      end
 
-    it('is not valid with a duplicate title') do
-      Post.create(user:, title: 'Test Post', content: 'Lorem ipsum' * 20)
-      post = Post.new(user:, title: 'Test Post', content: 'Lorem ipsum' * 20)
-      expect(post).to_not be_valid
+      it('is not valid with a content less than 100 characters') do
+        post = FactoryBot.build(:post, user:, title: 'Test Post', content: 'a' * 50)
+        expect(post).to_not be_valid
+      end
     end
+  end
 
-    it('is not valid with a content less than 100 characters') do
-      post = Post.new(user:, title: 'Test Post', content: 'a' * 30)
-      expect(post).to_not be_valid
-    end
+  describe('associations') do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
   end
 end

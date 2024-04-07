958bd00bfa807874cf3e3112b8d3f7b4a0786292
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-07 18:43:05 +0200

    tests: add delete_post_spec for testing post deletion functionality

---
+++ spec/system/posts/delete_post_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Delete post', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let!(:post) { FactoryBot.create(:post, user:) }
+
+  before do
+    login_as(user)
+    visit(posts_path)
+  end
+
+  context('from root page') do
+    it('deletes a post') do
+      dropdown_button = find("//button[@name='post_#{post.id}_controls']")
+      dropdown_button.click
+
+      click_on('delete')
+
+      expect(page).to have_content('Post was successfully deleted.')
+      expect(page).not_to have_content(post.title)
+    end
+  end
+
+  context('from edit page') do
+    before do
+      visit(posts_path)
+
+      find("//a[@href='/posts/#{post.id}']").click
+      expect(page).to have_content(post.title)
+
+      click_on('edit')
+      expect(page).to have_content("Edit post #{post.title}")
+    end
+
+    it('deletes a post') do
+      click_on('Delete')
+
+      expect(page).to have_content('Post was successfully deleted.')
+      expect(page).not_to have_content(post.title)
+    end
+  end
+end

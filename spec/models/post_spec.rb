62b8cb18d2ece19226a93abc43ec769c16eb92f0
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-07 13:55:59 +0200

    refactor: Post model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,44 +3,47 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   let!(:user) { FactoryBot.create(:user) }
 
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:content) }
-
   describe('validations') do
-    context('with valid attributes') do
-      let(:post) { FactoryBot.build(:post, user:, title: 'Test Post', content: 'Lorem ipsum' * 25) }
+    let(:valid_attributes) { { user:, title: 'Test Post', content: 'Lorem ipsum' * 25 } }
 
-      it { expect(post).to be_valid }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:content) }
+
+    context('with valid attributes') do
+      it('is valid') do
+        post = FactoryBot.build(:post, valid_attributes)
+        expect(post).to be_valid
+      end
     end
 
     context('with invalid attributes') do
       it('is not valid without a title') do
-        post = FactoryBot.build(:post, user:, title: nil)
+        post = FactoryBot.build(:post, valid_attributes.merge(title: nil))
         expect(post).to_not be_valid
       end
 
       it('is not valid without content') do
-        post = FactoryBot.build(:post, user:, content: nil)
+        post = FactoryBot.build(:post, valid_attributes.merge(content: nil))
         expect(post).to_not be_valid
       end
 
       it('is not valid without a user') do
-        post = FactoryBot.build(:post, user: nil)
+        post = FactoryBot.build(:post, valid_attributes.merge(user: nil))
         expect(post).to_not be_valid
       end
 
       it('is not valid with a title less than 5 characters') do
-        post = FactoryBot.build(:post, user:, title: 'Test', content: 'Lorem ipsum' * 25)
+        post = FactoryBot.build(:post, valid_attributes.merge(title: 'Test'))
         expect(post).to_not be_valid
       end
 
       it('is not valid with a title more than 200 characters') do
-        post = FactoryBot.build(:post, user:, title: 'a' * 201, content: 'Lorem ipsum' * 25)
+        post = FactoryBot.build(:post, valid_attributes.merge(title: 'a' * 201))
         expect(post).to_not be_valid
       end
 
       it('is not valid with a content less than 100 characters') do
-        post = FactoryBot.build(:post, user:, title: 'Test Post', content: 'a' * 50)
+        post = FactoryBot.build(:post, valid_attributes.merge(content: 'a' * 50))
         expect(post).to_not be_valid
       end
     end

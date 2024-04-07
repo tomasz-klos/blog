410c3bb3714a17eddda2f3a63ac38fe71751729b
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-07 20:25:05 +0200

    feat: create custom alert component

---
+++ app/components/common/alert/component.rb
@@ -0,0 +1,4 @@
+class Common::Alert::Component < ApplicationComponent
+  option :type, Types::String
+  option :message, Types::String
+end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,12 +2,13 @@ import { Application } from "@hotwired/stimulus";
 
 const application = Application.start();
 
-import { Toggle, Dropdown } from "tailwindcss-stimulus-components";
+import { Alert, Toggle, Dropdown } from "tailwindcss-stimulus-components";
 
 // Configure Stimulus development experience
 application.debug = false;
 window.Stimulus = application;
 
+application.register('alert', Alert)
 application.register("toggle", Toggle);
 application.register('dropdown', Dropdown);
 

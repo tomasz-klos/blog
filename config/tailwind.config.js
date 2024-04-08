ce232107559f57b7a60df3a956c4f0796f6eb0bc
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-08 18:28:41 +0200

    feat: update Tailwind CSS styles and configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -30,6 +30,14 @@ module.exports = {
         "fade-out": "fade-out 0.3s ease-in-out",
       },
     },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),

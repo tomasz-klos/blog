bef86c63e17f578e9db064a98e6e6d3ee0148f9f
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-08 19:10:39 +0200

    refactor: format files

--- app/components/post/component.rb
+++ app/components/post/component.rb
@@ -1,6 +1,6 @@
 class Post::Component < ApplicationComponent
   option :post, Types.Instance(Post)
-  option :current_user, Types.Instance(User)
+  option :current_user
 
   def author?
     post.user == current_user

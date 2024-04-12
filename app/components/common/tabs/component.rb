e3d730d2bcc5a812a51fa80d2728b179002a2bee
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-12 21:40:23 +0200

    feat: update Common::Tabs component and related components

--- app/components/common/tabs/component.rb
+++ app/components/common/tabs/component.rb
@@ -2,7 +2,7 @@ class Common::Tabs::Component < ApplicationComponent
   include Common::Tabs
   option :tabs_id, default: -> { SecureRandom.hex(10) }, optional: true
 
-  renders_many :buttons, lambda { |disabled:, id:, class_name: '', is_active: false|
+  renders_many :buttons, lambda { |id:, disabled: false, class_name: '', is_active: false|
                            Button::Component.new(class_name:, disabled:, id:, is_active:)
                          }
 

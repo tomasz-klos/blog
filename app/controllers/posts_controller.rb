670d299b7f6aa59985d6f6fb790becd91e379dd7
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-12 14:18:50 +0200

    refactor: display only published post on posts index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action(:authorize_user!, only: %i[edit update destroy])
 
   def index
-    @posts = Post.all
+    @posts = Post.published.order(created_at: :desc)
   end
 
   def show

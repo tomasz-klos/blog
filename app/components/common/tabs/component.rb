912794f41cc0d700f08cc8a6bcc2688468efe869
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-14 16:23:13 +0200

    refactor: update tabs and related components

--- app/components/common/tabs/component.rb
+++ app/components/common/tabs/component.rb
@@ -1,10 +1,12 @@
 class Common::Tabs::Component < ApplicationComponent
   include Common::Tabs
+
+  option :class_name, Types::String.default(''), optional: true
   option :tabs_id, default: -> { SecureRandom.hex(10) }, optional: true
 
   renders_many :buttons, lambda { |id:, disabled: false, class_name: '', is_active: false|
                            Button::Component.new(class_name:, disabled:, id:, is_active:)
                          }
 
-  renders_many :panels, ->(id:) { Panel::Component.new(id:) }
+  renders_many :panels, ->(id:, class_name: '') { Panel::Component.new(id:, class_name:) }
 end
--- app/components/common/tabs/panel/component.rb
+++ app/components/common/tabs/panel/component.rb
@@ -1,5 +1,6 @@
 class Common::Tabs::Panel::Component < ApplicationComponent
   include Common::Tabs::Panel
 
+  option :class_name, Types::String.default(''), optional: true
   option :id, Types::String
 end

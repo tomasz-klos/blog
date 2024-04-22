fd73a196be997a3849320720f8728fc729135a85
Tomasz Klos <tomasz.klos@nerds.family>
2024-04-22 17:45:19 +0200

    refactor: options in common tabs components

--- app/components/common/breadcrumb/component.rb
+++ app/components/common/breadcrumb/component.rb
@@ -1,4 +1,4 @@
 class Common::Breadcrumb::Component < ApplicationComponent
-  option :class_name, Types::String.default(''), optional: true
-  option :items, Types::Array.of(Types::Hash).default([])
+  option :class_name, optional: true
+  option :items, Types::Array.of(Types::Hash)
 end

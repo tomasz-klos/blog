0987aaafba4324d53b617d7a2178312d43adb7ec
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-01 14:45:17 +0200

    refactor: delete_post_spec.rb

--- spec/system/posts/delete_post_spec.rb
+++ spec/system/posts/delete_post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'Delete post', type: :system do
-  let(:user) { FactoryBot.create(:user) }
+RSpec.describe('Delete post', type: :system, js: true) do
+  let!(:user) { FactoryBot.create(:user) }
   let!(:post) { FactoryBot.create(:post, user:) }
 
   before do
@@ -9,9 +9,18 @@ RSpec.describe 'Delete post', type: :system do
     visit(dashboard_posts_path)
   end
 
-  shared_examples('deleting a post') do |delete_button|
-    it do
-      click_on(delete_button)
+  context('from dashboard posts page') do
+    it('deletes post') do
+      dropdown_button = find("button[id='post_#{post.id}_controls']")
+      dropdown_button.click
+
+      expect(page).to have_selector("#post_#{post.id}_menu")
+
+      within("#post_#{post.id}_menu") do
+        click_on('Delete')
+      end
+
+      expect(page).to have_content('Are you sure you want to delete this post?')
 
       within('dialog') do
         find("button[value='confirm']").click
@@ -22,21 +31,21 @@ RSpec.describe 'Delete post', type: :system do
     end
   end
 
-  context('from dashboard posts page') do
-    before do
-      dropdown_button = find("button[name='post_#{post.id}_controls']")
-      dropdown_button.click
-    end
-
-    it_behaves_like('deleting a post', 'Delete')
-  end
-
   context('from edit page') do
-    before do
+    it('deletes post') do
       click_on(post.title)
       expect(page).to have_content('Edit post')
-    end
 
-    it_behaves_like('deleting a post', 'Delete post')
+      click_on('Delete post')
+
+      expect(page).to have_content('Are you sure you want to delete this post?')
+
+      within('dialog') do
+        find("button[value='confirm']").click
+      end
+
+      expect(page).to have_content('Post was successfully deleted.')
+      expect(page).not_to have_content(post.title)
+    end
   end
 end

79348ea5c3343a2e2c71748333b3b1f57aadac2c
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-01 20:33:18 +0200

    feat: add name column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 end

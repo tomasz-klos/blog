977bfcd697a1ec60370f04310a1f67ce5de88d87
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-08 15:22:29 +0200

    feat: display user stats in user profile view

--- app/controllers/users_profiles_controller.rb
+++ app/controllers/users_profiles_controller.rb
@@ -2,5 +2,6 @@ class UsersProfilesController < ApplicationController
   def show
     @user = User.friendly.find(params[:slug])
     @posts = @user.posts.published
+    @comments = Comment.where(user: @user)
   end
 end

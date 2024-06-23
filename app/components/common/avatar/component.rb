1c51edc18b0e813fa025dfe4923eb6b5d16d1aaa
Tomasz Klos <tomasz.klos@nerds.family>
2024-06-23 15:51:36 +0200

    refactor: Update avatar component to support additional avatar sizes

--- app/components/common/avatar/component.rb
+++ app/components/common/avatar/component.rb
@@ -7,9 +7,15 @@ class Common::Avatar::Component < ApplicationComponent
     {
       "sm" => "size-5",
       "md" => "size-8",
-      "lg" => "size-16",
-      "xl" => "size-24",
-      "2xl" => "size-32"
+      "lg" => "size-12",
+      "xl" => "size-16",
+      "2xl" => "size-24",
+      "3xl" => "size-32",
+      "4xl" => "size-40",
+      "5xl" => "size-48",
+      "6xl" => "size-56",
+      "7xl" => "size-64",
+      "8xl" => "size-72"
     }[size]
   end
 end
